27cac62821c95a6f0a42f1b1dde2c8717fa88005
Muhammad Ilham Syarifuddin <muhilhamsyarifuddin@gmail.com>
2018-11-09 18:23:47 +0700

    change source IP

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,7 +5,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->helper('url');
-		$json = file_get_contents('http://localhost/cirest/index.php/article');
+		$json = file_get_contents('http://172.168.100.2/cirest/index.php/article');
 		$data['obj'] = json_decode($json);
 		// echo "<pre>"; print_r($obj);echo "</pre>";
 		$this->load->view('V_home', $data);
